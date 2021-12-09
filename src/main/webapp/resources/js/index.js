6c7effc84ac0e803a394d9ec4280f4d315a5b8d6
Azevedo-Erick <azevedoerick762@gmail.com>
2021-12-09 10:46:07 -0300

    Segregação dos js

--- src/main/webapp/resources/js/index.js
+++ src/main/webapp/resources/js/index.js
@@ -48,26 +48,6 @@ function randomGenderColor(){
 	}
 }
 
-const inputs = {
-	envelope:document.getElementById("envelope"),
-	lock:document.getElementById("lock"),
-}
-
-inputs.envelope.addEventListener("mouseover", ()=>{
-	inputs.envelope.firstElementChild.className = "fa fa-envelope-open icon";
-});
-
-inputs.envelope.addEventListener("mouseleave", ()=>{
-	inputs.envelope.firstElementChild.className = "fa fa-envelope icon";
-});
-
-inputs.lock.addEventListener("mouseover", ()=>{
-	inputs.lock.firstElementChild.className = "fa fa-unlock icon";
-});
-
-inputs.lock.addEventListener("mouseleave", ()=>{
-	inputs.lock.firstElementChild.className = "fa fa-lock icon";
-});
 function onPageLoad(){
 	randomGenderColor();
 	trsAddEvent();
