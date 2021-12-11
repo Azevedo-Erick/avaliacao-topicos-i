27bc38381114c1e3f780812f6489a682af1b95c5
Azevedo-Erick <azevedoerick762@gmail.com>
2021-12-10 23:10:14 -0300

    Separacao do js

--- src/main/webapp/resources/js/index.js
+++ src/main/webapp/resources/js/index.js
@@ -1,48 +1,3 @@
-const cad = document.getElementById("cadastrar");
-
-cad.addEventListener("mouseover",()=>{
-	let element = document.querySelectorAll("#cadastro-container > .fake")
-	document.getElementById("angle-cad").className = "fa fa-angle-down";
-	element.forEach((e)=>{
-		
-	e.style.display = "flex";
-	})
-})
-
-const nav = document.getElementById("cadastro-container");
-
-nav.addEventListener("mouseleave",()=>{
-	let element = document.querySelectorAll("#cadastro-container > .fake")
-	document.getElementById("angle-cad").className = "fa fa-angle-up";
-	element.forEach((e)=>{
-		
-	e.style.display = "none";
-	})
-})
-
-
-const log = document.getElementById("login-nav-container");
-
-
-log.addEventListener("mouseover",()=>{
-	let element = document.querySelectorAll("#login-nav-container > .fake")
-	document.getElementById("angle-log").className = "fa fa-angle-down";
-	element.forEach((e)=>{
-		
-	e.style.display = "flex";
-	})
-})
-
-const logWrap = document.getElementById("login-wrapper");
-
-logWrap.addEventListener("mouseleave",()=>{
-	let element = document.querySelectorAll("#login-nav-container > .fake")
-	document.getElementById("angle-log").className = "fa fa-angle-up";
-	element.forEach((e)=>{
-	e.style.display = "none";
-	})
-})
-
 const trs = document.getElementsByTagName("tr");
 
 function trsAddEvent(){
@@ -76,3 +31,5 @@ function onPageLoad(){
 	trsAddEvent();
 }
 document.onload = onPageLoad();
+
+
